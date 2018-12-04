98d54d6ef96c86b3913eefd1eb7b98adf942e961
robertoeugenio1978@gmail.com <robertoeugenio>
2018-12-04 14:19:59 -0200

    ajustes depois da correção do erro do erro que o angoti resolveu

--- app/src/main/java/com/example/rober/droidcafe/MainActivity.java
+++ app/src/main/java/com/example/rober/droidcafe/MainActivity.java
@@ -3,30 +3,31 @@ package com.example.rober.droidcafe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
- //DEFINIÇAO DA CHAVE PUTeXTRA
+
     public static final String EXTRA_MESSAGE =
             "com.example.android.droidcafe.extra.MESSAGE";
 
+
+    private String mOrderMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar =  findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-         //alterado metodo do anterior
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,52 +49,33 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
-    // adicionando metodo mensagem
+
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message,
                 Toast.LENGTH_SHORT).show();
     }
 
-    //metodo clicar
-    /**
-     /
-     * Shows a message that the donut image was clicked.
-     alterando
-     */
     public void showDonutOrder(View view) {
         mOrderMessage = getString(R.string.donut_order_message);
         displayToast(mOrderMessage);
     }
-    /**
-     * Shows a message that the ice cream sandwich image was clicked.
-     testando git
-     */
+
     public void showIceCreamOrder(View view) {
         mOrderMessage = getString(R.string.ice_cream_order_message);
         displayToast(mOrderMessage);
     }
 
-    /**
-     * Shows a message that the froyo image was clicked.
-     */
     public void showFroyoOrder(View view) {
         mOrderMessage = getString(R.string.froyo_order_message);
         displayToast(mOrderMessage);
     }
- //criando variavel
-    private String mOrderMessage;
+
 }
- /// observar o icone do carrinho
-// e fazer o desafio
